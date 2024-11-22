47215e3bb78828605d1585bd948616f6add76130
Fernando <ufernando769@gmail.com>
2024-11-21 21:36:47 -0600

    Configure SECRET_KEY and DEBUG for Render deployment

--- proyecto_licitaciones/settings.py
+++ proyecto_licitaciones/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!da6=n%g1mzxv0hignu!*_q1-i42h@)f0zy5th!5evbtb=$s*w'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-!da6=n%g1mzxv0hignu!*_q1-i42h@)f0zy5th!5evbtb=$s*w')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['*']  # Permite todos los hosts temporalmente para pruebas
+ALLOWED_HOSTS = ['proyecto-licitaciones.onrender.com']
 
 
 # Application definition
